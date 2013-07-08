7bce119b94db25a459d71e77c1708c61136ca377
Christopher Wright <cmwright33@gmail.com>
2013-07-08 19:28:18 -0400

    deleted the app because it was in rails 4 because i bundle installed from the original assiginment

--- rails_todo/config/initializers/secret_token.rb
+++ rails_todo/config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-RailsTodo::Application.config.secret_key_base = 'b131c77ceeb342a8d40cc64b3a091b64b55403005a75d3ddc253117c84eb0eb7fbf04d39660b5bcf620fdc40ac4f86b612f2950acf2e634162ab6555f95893af'
+RailsTodo::Application.config.secret_token = 'c8da3e341d447b69c60ad95dfdc8a54b38ac024b4ec87a0056dac183544f7ca65d8abd5b441a6afc741843943f29468f96e71c5c9611a89e3a54cb02d5d4a6ee'
