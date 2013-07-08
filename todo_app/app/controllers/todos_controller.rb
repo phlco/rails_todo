ad64e99a9010fd92507db539775533a97cea8e05
mvanger <mvanger@nyu.edu>
2013-07-08 17:41:42 -0400

    added stuff for a new todo

--- todo_app/app/controllers/todos_controller.rb
+++ todo_app/app/controllers/todos_controller.rb
@@ -4,6 +4,21 @@ class TodosController < ApplicationController
     @todos = Todo.all
   end
 
+  def new
+  end
+
+  def create
+    task = params[:task]
+    description = params[:description]
+    due_date = params[:due_date]
+    todo = Todo.new
+    todo.task = task
+    todo.description = description
+    todo.due_date = due_date
+    todo.save
+    redirect_to('/todos')
+  end
+
   def show
     @todo = Todo.find(params[:id])
   end
--- todo_app/config/routes.rb
+++ todo_app/config/routes.rb
@@ -1,5 +1,7 @@
 TodoApp::Application.routes.draw do
   root :to => 'welcome#index'
   get '/todos' => 'todos#index'
+  post '/todos' => 'todos#create'
+  get '/todos/new' => 'todos#new'
   get '/todos/:id' => 'todos#show'
 end
