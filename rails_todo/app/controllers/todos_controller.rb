8cec1839408a673e7e483fdba2c9865983de72c3
Christopher Wright <cmwright33@gmail.com>
2013-07-08 18:49:09 -0400

    created route for post added them to the new.html.erb

--- rails_todo/app/controllers/todos_controller.rb
+++ rails_todo/app/controllers/todos_controller.rb
@@ -7,4 +7,11 @@ class TodosController < ApplicationController
   def new
 
   end
+
+  def create
+    todo = TodoTable.new
+    todo.task = params[:task]
+    todo.task_description = params[:task_description]
+    todo.save
+  end
 end
--- rails_todo/config/routes.rb
+++ rails_todo/config/routes.rb
@@ -3,6 +3,7 @@ RailsTodo::Application.routes.draw do
   root :to => 'welcome#index'
   get '/todos' => 'todos#index'
   get '/todos/new' => 'todos#new'
+  post '/todos' =>'todos#create'
 
 
 
