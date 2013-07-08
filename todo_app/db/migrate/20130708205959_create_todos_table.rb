2c117c3769ccf867c703ada38822f14854fc0fe4
mvanger <mvanger@nyu.edu>
2013-07-08 17:02:47 -0400

    created table

--- todo_app/db/migrate/20130708205959_create_todos_table.rb
+++ todo_app/db/migrate/20130708205959_create_todos_table.rb
@@ -1,6 +1,10 @@
 class CreateTodosTable < ActiveRecord::Migration
   def change
     create_table :todos_tables do |t|
+      t.string :task
+      t.string :description
+      t.string :due_date
+      t.boolean :completed, default: false
     end
   end
 end
