cbe25daa8547acafd92a9b859e0124b3037e3bce
Alina Guzman <alinochka8@gmail.com>
2013-07-08 19:51:35 -0400

    Added function to create task

--- rails_todo/app/controllers/tasks_controller.rb
+++ rails_todo/app/controllers/tasks_controller.rb
@@ -2,4 +2,16 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+
+  end
+
+  def create
+    new_task = Task.new
+    new_task.name = params[:name]
+    new_task.description = params[:description]
+    new_task.save
+    redirect_to '/tasks'
+  end
 end
