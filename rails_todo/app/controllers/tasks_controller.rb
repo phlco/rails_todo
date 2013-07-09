0f83186988cebc48a0529577ff97c92689d70522
Alina Guzman <alinochka8@gmail.com>
2013-07-08 20:30:51 -0400

    Added functions to edit and delete task

--- rails_todo/app/controllers/tasks_controller.rb
+++ rails_todo/app/controllers/tasks_controller.rb
@@ -14,4 +14,26 @@ class TasksController < ApplicationController
     new_task.save
     redirect_to '/tasks'
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.name = params[:name]
+    task.description = params[:description]
+    task.save
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to '/tasks'
+  end
 end
