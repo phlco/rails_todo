11ef931e95ff81b8501de030490b4b9a489d9032
Beryl <beryl.schragger@gmail.com>
2013-07-09 00:15:46 -0400

    Complete edit task and show task functions and methods.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -17,14 +17,26 @@ class TodosController < ApplicationController
   end
 
   def show
+    @todo = Todo.find(params[:id])
   end
 
   def edit
+    @todo = Todo.find(params[:id])
   end
 
   def update
+    todo = Todo.find(params[:id])
+    todo.name = params[:name]
+    todo.description = params[:description]
+    todo.do_by = params[:do_by]
+    todo.done = params[:done]
+    todo.save
+    redirect_to "/todos/#{todo.id}"
   end
 
   def destroy
+    todo = Todo.find(params[:id])
+    todo.destroy
+    redirect_to "/todos"
   end
 end
