1bf02852935961640ad047001eb0563c79e0ae1d
Zachariah Reitano <zreitano@gmail.com>
2013-07-09 17:57:25 -0400

    CRUD for Contacts

--- todos/app/controllers/contacts_controller.rb
+++ todos/app/controllers/contacts_controller.rb
@@ -1,6 +1,38 @@
 class ContactsController < ApplicationController
 
   def index
+    @contacts = Contact.all
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    contact = Contact.new
+    contact.name = params[:name]
+    contact.save
+    redirect_to '/contacts'
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    contact = Contact.find(params[:id])
+    contact.name = params[:name]
+    contact.save
+    redirect_to('/contacts')
+  end
+
+  def destroy
+    contact = Contact.find(params[:id])
+    contact.destroy
+    redirect_to('/contacts')
   end
 
 end
--- todos/config/routes.rb
+++ todos/config/routes.rb
@@ -12,6 +12,13 @@ Todos::Application.routes.draw do
   put '/todos/:id' => 'todos#update'
   delete '/todos/:id' => 'todos#destroy'
 
-
+  ###Contacts###
+  get '/contacts' => 'contacts#index'
+  get '/contacts/new' => 'contacts#new'
+  get '/contacts/:id' => 'contacts#show'
+  post '/contacts' => 'contacts#create'
+  get '/contacts/:id/edit' => 'contacts#edit'
+  put '/contacts/:id' => 'contacts#update'
+  delete '/contacts/:id' => 'contacts#destroy'
 end
 
