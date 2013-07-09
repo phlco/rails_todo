87d10282d60281770d357a1a4692c0465b2b5de4
Christine Coulter <christinetcoulter@gmail.com>
2013-07-08 23:52:19 -0400

    Create new rails todos. Transferred content from older version (it was Rails 4. sadface.) to the new rails todos (rails 3.2.13).

--- todos/config/initializers/secret_token.rb
+++ todos/config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Todos::Application.config.secret_key_base = '16aaf61fda10bc6d79c02ee3a4514f0b7325d8c24b79f4235b19d41d85891a403046abe3cc2407137d713be3bb539b78cff29cda8166959a06b3999dc4203799'
+Todos::Application.config.secret_token = '1774cd4d2b6d5b548a9020bb441ee67e05eaf103fb45318f99354f180724148b342ba90257b313d8c54bf60e40298ec0de08d26030ae27fd9f651644a63f1039'
