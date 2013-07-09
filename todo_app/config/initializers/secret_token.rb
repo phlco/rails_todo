58e097e979dcafe0c63a50ca49cfeb04bdbdd5a8
mvanger <mvanger@nyu.edu>
2013-07-08 20:12:00 -0400

    Fixed rails versions, can add new todos, and view existing ones

--- todo_app/config/initializers/secret_token.rb
+++ todo_app/config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-TodoApp::Application.config.secret_key_base = '4234496799df63f84f0d34767fb7a7d8942faf90901d3905322d5323c4efca0cb6f393ebccfeede66e7ed36fae7bac3321cf01fa50c42f6987d6c035298d83cf'
+TodoApp::Application.config.secret_token = '12f3a947f696ba9cce3705512ad9cbfb28098584f58ce76223695cd27d2e77c8be0ee8421bfa3c23de57bcb7b057d70cdd025a858f0dfe01f3aeb64e083396ad'
