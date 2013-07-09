61f7cfd571ab3db398fb284503431317cdbb377b
Jamal Powell <jamalkpowell@gmail.com>
2013-07-08 22:33:47 -0400

    Modify Todos Controller

--- rails_todo/app/controllers/todos_controller.rb
+++ rails_todo/app/controllers/todos_controller.rb
@@ -5,14 +5,21 @@ class TodosController < ApplicationController
   end
 
   def create
-
+    todo = Todo.find(params[:id])
   end
 
   def show
+    name = params[:name]
+    description = params[:description]
     @todo = Todo.find(params[:id])
   end
 
   def update
+    todo = Todo.find(params[:id])
+    todo.name = params[:name]
+    todo.description = params[:age]
+    todo.save
+    redirect_to "/todos/#{todo.id}"
 
   end
 
@@ -21,6 +28,8 @@ class TodosController < ApplicationController
   end
 
   def destroy
-
+    todo = Todo.find(params[:id])
+    todo.destroy
+    redirect_to '/todos'
   end
 end
