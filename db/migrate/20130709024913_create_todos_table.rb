11717fd3211d2289c63fe7de01321a0584175b0f
Beryl <beryl.schragger@gmail.com>
2013-07-08 23:12:16 -0400

    Add todos table.

---
+++ db/migrate/20130709024913_create_todos_table.rb
@@ -0,0 +1,14 @@
+class CreateTodosTable < ActiveRecord::Migration
+  def up
+    create_table :todos do |t|
+      t.string :name
+      t.string :description
+      t.string :do_by
+      t.boolean :done
+      t.timestamps
+  end
+
+  def down
+      drop_table :todos
+  end
+end
