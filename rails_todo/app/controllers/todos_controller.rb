b36d87f43686ffd938d508b97a6c8bb0cc5d1e5b
Jamal Powell <jamalkpowell@gmail.com>
2013-07-08 22:00:44 -0400

    add show & new files

--- rails_todo/app/controllers/todos_controller.rb
+++ rails_todo/app/controllers/todos_controller.rb
@@ -3,4 +3,24 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
   end
+
+  def create
+
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def destroy
+
+  end
 end
