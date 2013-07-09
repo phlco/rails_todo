2155cd74bf999edafd8f1bf553eae262371ddb8d
eleyna <eleyna@gmail.com>
2013-07-08 22:21:44 -0400

    done

--- rails_todo/config/routes.rb
+++ rails_todo/config/routes.rb
@@ -1,3 +1,10 @@
 RailsTodo::Application.routes.draw do
-  root :to => 'welcome#index'
+  root :to => 'todos#index'
+  get '/todos/new' => 'todos#new'
+  post '/todos' => 'todos#create'
+  get '/todos' => 'todos#index'
+  get '/todos/:id' => 'todos#show'
+  get '/todos/:id/edit' => 'todos#edit'
+  put '/todos/:id' => 'todos#update'
+  delete '/todos/:id' => 'todos#delete'
 end
